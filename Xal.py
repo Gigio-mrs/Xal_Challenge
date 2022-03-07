47ffe89638023af82cd4a773390ddae7c5e8060b
Gigio-mrs <69736383+Gigio-mrs@users.noreply.github.com>
2022-03-06 18:09:16 -0600

    Update

--- Xal.py
+++ Xal.py
@@ -2,5 +2,37 @@ import json
 
 with open(r'C:\Users\co99d\OneDrive\Escritorio\xal.json') as file:
     data = json.load(file)
-sorted_json_data =data.sort(key = lambda x:x["time"])
-print(sorted_json_data)
+    sum = 0
+    count = 0
+    minimum = []
+    old = []
+    rep = []
+    ans = {}
+    for key in data["items"]:
+
+        minimum.append(key["view_count"])
+
+        old.append(key["last_activity_date"])
+        
+        rep.append(key["owner"]["reputation"])
+        
+        if "accepted_answer_id" in key:
+            ans[key["owner"]["reputation"]]=key["accepted_answer_id"]
+            print(key["accepted_answer_id"])
+
+        if key["is_answered"] == False:
+            count += 1
+        elif key["is_answered"] == True:
+            sum += key["answer_count"]
+        
+
+    print("numero de respuestas contestadas: "+ str(sum))
+    print("numero de respuestas no contestadas: " + str(count))
+    print("respuesta con el menor numero de vistas: " + str(min(minimum)))
+    print("respuesta mas vieja: " + str(min(old)))
+    print("respuesta mas nueva: " + str(max(old)))
+    print("reputacion mas alta: " + str(max(rep)))
+    print ("Respuesta a la reputacion mas alta: "+ str(ans[max(rep)]))
+    
+
+    
